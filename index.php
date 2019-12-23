96f84de66654eec660ba666aad22193742b533ea
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-12-23 16:10:10 +0700

    Fix example in index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ use \InstagramScraper\Paginator;
 
 $paginator = new Paginator(2);
 $scraper = new TagScraper(NULL, $paginator);
-$tags = $scraper->scrape('snail', TagScraper::IMAGES);
-print_r($tags);
-echo count($tags);
+$tags = $scraper->scrape('snail');
+print_r($tags->videos());
+echo $tags->videos()->count();
